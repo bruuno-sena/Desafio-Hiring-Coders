83768cb1cfedc65616420480aea7937283100945
Bruno Sena <bsena8@hotmail.com>
2021-07-18 21:31:47 -0300

    Arquivos Desafio

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit',(e) =>
+ {
+    e.preventDefault();
+    
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead',convertData)
+
+    let content = document.getElementById('content')
+    let carregando = `<p>carregando...</p>`
+    
+    let pronto = `<p>E-mail cadastrado com sucesso! Você será rediricionado para nosso site!</p>`
+    content.innerHTML = pronto
+    
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+    }, 1000)
+})
+
+
+let time = 5000,
+currentImageIndex = 0,
+images = document
+            .querySelectorAll("#slider img")
+max = images.length;
+
+function nextImage() {
+
+images[currentImageIndex]
+    .classList.remove("selected")
+
+currentImageIndex++
+
+if(currentImageIndex >= max)
+    currentImageIndex = 0
+
+images[currentImageIndex]
+    .classList.add("selected")
+}
+
+function start() {
+setInterval(() => {
+    // troca de image
+    nextImage()
+}, time)
+}
+
+window.addEventListener("load", start)
